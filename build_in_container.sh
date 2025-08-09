3831bc92dc5e2bbfbd361f9ca97eca668f45b8e4
Helmut Konrad Schewe <helmutus@outlook.com>
2025-08-09 13:29:32 +0200

    fix(build): make script run

--- build_in_container.sh
+++ build_in_container.sh
@@ -5,11 +5,15 @@ rm -rf output || true
 mkdir -p output
 
 echo "=> Erzeuge PDF mit lualatex"
-lualatex -interaction=nonstopmode -halt-on-error -halt-on-warning -output-directory=output main.tex
+lualatex -interaction=nonstopmode -halt-on-error -output-directory=output main.tex
+if grep -q "Warning" output/main.log; then
+    echo "Warning detected! Stopping compilation."
+    exit 1
+fi
 lualatex -interaction=nonstopmode -halt-on-error -output-directory=output main.tex || true
 
-echo "=> Erzeuge EPUB mit tex4ebook"
-tex4ebook -f epub3 -o output/book main.tex || true
+#echo "=> Erzeuge EPUB mit tex4ebook"
+#tex4ebook -f epub3 -o output/book main.tex || true
 
 if [ ! -f output/book.epub ]; then
   echo "=> Fallback: Pandoc-Konvertierung"
