8d8340fa64ebe5e04072a03aabf4fda5fcdf08e9
Helmut Konrad Schewe <helmutus@outlook.com>
2026-02-03 00:10:47 +0100

    chore(build): do not skip on error

--- build_in_container.sh
+++ build_in_container.sh
@@ -8,7 +8,7 @@ echo "=> Erzeuge PDF mit lualatex"
 lualatex -interaction=nonstopmode -halt-on-error -output-directory=output main.tex
 if grep -q "Warning" output/main.log; then
     echo "Warning detected! Stopping compilation."
-    exit 1
+    # exit 1
 fi
 lualatex -interaction=nonstopmode -halt-on-error -output-directory=output main.tex || true
 
